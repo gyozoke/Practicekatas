86c13e7e0fb5b70c21a530e31c1fb28384c5caa8
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2024-03-04 13:42:40 +0000

    complete multipleof3or5

--- __test__/multiplesof3or5.test.js
+++ __test__/multiplesof3or5.test.js
@@ -1,6 +1,8 @@
 const multipleof3or5 = require("../katas/multiplesof3or5");
 
 /*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3 5 6  and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
 - Should return a number
 - Should return 23 that is  multiples  of 3 and 5 below 10 
 */
@@ -16,7 +18,20 @@ describe("multipleof3or5", () => {
   });
   test("Should return 23 that is  multiples  of 3 and 5 below 10 ", () => {
     //arrange
+    const input = 10;
+    const expectedOutput = 23;
     //act
+    const result = multipleof3or5(input);
     //assert
+    expect(result).toBe(expectedOutput);
+  });
+  test("Should return 266333 that is multiple of 3 and 5 below 1000", () => {
+    //arrange
+    const input = 1000;
+    const expectedOutput = 266333;
+    //act
+    const result = multipleof3or5(input);
+    //assert
+    expect(result).toBe(expectedOutput);
   });
 });
--- katas/multiplesof3or5.js
+++ katas/multiplesof3or5.js
@@ -1,5 +1,27 @@
-function multipleof3or5() {
-  return 5;
+function multipleof3or5(num) {
+  const multiplesOfThree = [];
+  const multiplesOfFive = [];
+
+  for (let i = 3; i < num; i = i + 3) {
+    multiplesOfThree.push(i);
+  }
+
+  for (let i = 5; i < num; i = i + 5) {
+    multiplesOfFive.push(i);
+  }
+
+  const sumOfMultipleOfThree = multiplesOfThree.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const sumOfMultipleOfFive = multiplesOfFive.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  const result = sumOfMultipleOfThree + sumOfMultipleOfFive;
+
+  return result;
 }
 
 module.exports = multipleof3or5;
