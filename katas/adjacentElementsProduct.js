accce410ff7ae0aaafd867fbda3cbd633f9c58fd
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2024-03-05 02:31:30 +0000

    complete adjacentElementsProduct

--- katas/adjacentElementsProduct.js
+++ katas/adjacentElementsProduct.js
@@ -1,15 +1,18 @@
 function adjacentElementsProduct(arr) {
-  if (arr.length === 0 || arr.length === 1) return 0;
-  //   if ((arr.length === 2 && arr[0] === 0) || arr[1] === 0) {
-  //     return 0;
-  //   }
+  if (arr.length < 2) return 0;
+  let maxProduct = arr[0] * arr[1];
+  let maxPair = [arr[0], arr[1]];
+
   for (let i = 0; i < arr.length; i++) {
-    if (arr.length === 2 && arr[i] > 0) {
-      return arr[i] * arr[i];
-    } else if (arr[i] === 0) {
-      return 0;
+    const currentProduct = arr[i] * arr[i + 1];
+
+    if (currentProduct > maxProduct) {
+      maxProduct = currentProduct;
+      maxPair = [arr[i], arr[i + 1]];
     }
   }
+
+  return maxPair[0] * maxPair[1];
 }
 
 module.exports = adjacentElementsProduct;
